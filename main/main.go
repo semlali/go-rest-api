d4afd0ac74d69039dab311c039b142f3e80dd2bb
Samia.SEMLALI <Samia.SEMLALI@berger-levrault.com>
2025-03-27 11:30:23 +0000

    Update func userHandler to send message with variable user

--- main/main.go
+++ main/main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-// Struct pour les données que nous allons renvoyer dans notre API
-type Message struct {
-	Text string `json:"text"`
-}
-
 // Structure pour représenter un utilisateur
 type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   int
+	Name string
 }
 
 // Handler pour la route "/"
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// Définir le type de contenu comme "texte" et renvoyer un message simple
+	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-
-	// Création d'un message de réponse
-	response := Message{
-		Text: "Bonjour, bienvenu dans l'API Go!",
-	}
-
-	// Convertir la réponse en JSON et l'envoyer
-	err := json.NewEncoder(w).Encode(response)
-	if err != nil {
-		fmt.Println("error : ", err)
-	}
+	w.Write([]byte("Bonjour, bienvenue dans l'API Go en texte brut!"))
 }
 
 // Handler pour obtenir un utilisateur par ID
@@ -42,14 +27,16 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 		Name: "Samia Semlali",
 	}
 
+	// Définir le type de contenu comme "texte" pour une réponse en texte brut
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	// Convertir l'utilisateur en JSON et l'envoyer en réponse
-	err := json.NewEncoder(w).Encode(user)
-	if err != nil {
-		fmt.Println("error : ", err)
-	}
+	// Créer un message avec les informations de l'utilisateur
+	message := fmt.Sprintf("Utilisateur : %s, ID : %d", user.Name, user.ID)
+
+	// Envoyer le message
+	w.Write([]byte(message))
+
 }
 
 func main() {
